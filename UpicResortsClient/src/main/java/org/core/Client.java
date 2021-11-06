2788d475e389f459050b5b481a43f9cf7e7e07f4
sohilpyati <pyati.s@husky.neu.edu>
2021-11-06 19:37:16 -0400

    Changes to client

--- UpicResortsClient/src/main/java/org/core/Client.java
+++ UpicResortsClient/src/main/java/org/core/Client.java
@@ -10,95 +10,97 @@ import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 public class Client {
-    private static final Logger logger = LogManager.getLogger(Client.class);
-    // parameters ideally in env, but here for testing
-    public int numThreads = 32;
-    public int numSkiers = 10000;
-    public int numLifts = 40;
-    public int numRuns = 20;
 
-    public static String IP = "http://18.117.216.20:8080";
+  private static final Logger logger = LogManager.getLogger(Client.class);
+  // parameters ideally in env, but here for testing
+  public int numThreads = 32;
+  public int numSkiers = 10000;
+  public int numLifts = 40;
+  public int numRuns = 20;
 
+  public static String IP = "http://localhost:8080";
+
+  //phase 2 starts after 10% of part 1 is finished
+  public final CountDownLatch phase1;
+  //phase 3 starts after 10% of total threads finished
+  public final CountDownLatch phase2;
+  public final CountDownLatch total;
+
+  // success and failure checks for each thread
+  public int success = 0;
+  public int failure = 0;
+
+  public Client() {
     //phase 2 starts after 10% of part 1 is finished
-    public final CountDownLatch phase1;
-    //phase 3 starts after 10% of total threads finished
-    public final CountDownLatch phase2;
-    public final CountDownLatch total;
+    phase1 = new CountDownLatch(numThreads / 4 / 10);
 
-    // success and failure checks for each thread
-    public int success = 0;
-    public int failure = 0;
+    phase2 = new CountDownLatch(numThreads / 10);
 
-    public Client() {
-        //phase 2 starts after 10% of part 1 is finished
-        phase1 = new CountDownLatch(numThreads / 4 / 10);
+    total = new CountDownLatch(numThreads / 4 + numThreads + numThreads / 4);
+  }
 
-        phase2 = new CountDownLatch(numThreads / 10);
-
-        total = new CountDownLatch(numThreads / 4 + numThreads + numThreads / 4);
+  public Client(int numThreads, int numSkiers, int numLifts, int numRuns, String ip) {
+    if (numThreads < 0 || numThreads > 256) {
+      throw new IllegalArgumentException("Numthreads should be between 0 and 256");
     }
-
-    public Client(int numThreads, int numSkiers, int numLifts, int numRuns, String ip) {
-        if (numThreads < 0 || numThreads > 256) {
-            throw new IllegalArgumentException("Numthreads should be between 0 and 256");
-        }
-        if (numSkiers < 0 || numSkiers > 50000) {
-            throw new IllegalArgumentException("NumSkiers should be between 0 and 50000");
-        }
-        if (numLifts < 5 || numLifts > 60) {
-            throw new IllegalArgumentException("NumLifts should be between 5 and 60");
-        }
-        if (numRuns < 0 || numRuns > 20) {
-            throw new IllegalArgumentException("NumRuns should be between 1 and 20");
-        }
-        this.numThreads = numThreads;
-        this.numSkiers = numSkiers;
-        this.numLifts = numLifts;
-        this.numRuns = numRuns;
-        this.IP = ip;
-        //phase 2 starts after 10% of part 1 is finished
-        phase1 = new CountDownLatch(numThreads / 4 / 10);
-
-        phase2 = new CountDownLatch(numThreads / 10);
-
-        total = new CountDownLatch(
-                numThreads / 4 + numThreads + numThreads / 4);
+    if (numSkiers < 0 || numSkiers > 50000) {
+      throw new IllegalArgumentException("NumSkiers should be between 0 and 50000");
     }
-
-    public synchronized void testSuccess() {
-        success++;
+    if (numLifts < 5 || numLifts > 60) {
+      throw new IllegalArgumentException("NumLifts should be between 5 and 60");
     }
-
-    public synchronized void testFailure() {
-        failure++;
+    if (numRuns < 0 || numRuns > 20) {
+      throw new IllegalArgumentException("NumRuns should be between 1 and 20");
     }
+    this.numThreads = numThreads;
+    this.numSkiers = numSkiers;
+    this.numLifts = numLifts;
+    this.numRuns = numRuns;
+    this.IP = ip;
+    //phase 2 starts after 10% of part 1 is finished
+    phase1 = new CountDownLatch(numThreads / 4 / 10);
 
-    public synchronized int getSuccess() {
-        return success;
+    phase2 = new CountDownLatch(numThreads / 10);
+
+    total = new CountDownLatch(
+        numThreads / 4 + numThreads + numThreads / 4);
+  }
+
+  public synchronized void testSuccess() {
+    success++;
+  }
+
+  public synchronized void testFailure() {
+    failure++;
+  }
+
+  public synchronized int getSuccess() {
+    return success;
+  }
+
+  public synchronized int getFailure() {
+    return failure;
+  }
+
+
+  public static void main(String[] args) throws InterruptedException {
+    Client client = new Client();
+    if (args.length == 4) {
+      client = new Client(
+          Integer.parseInt(args[0]),
+          Integer.parseInt(args[1]),
+          Integer.parseInt(args[2]),
+          Integer.parseInt(args[3]),
+          args[4]);
     }
+    //client = new Client(1, 1, 16, 1, Client.IP);
+    Logger.getRootLogger()
+        .addAppender(new ConsoleAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
+    final int skiersPerThread = client.numSkiers / client.numThreads;
+    final Random random = new Random();
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
-    public synchronized int getFailure() {
-        return failure;
-    }
-
-
-    public static void main(String[] args) throws InterruptedException {
-        Client client = new Client();
-        if (args.length == 4) {
-            client = new Client(
-                    Integer.parseInt(args[0]),
-                    Integer.parseInt(args[1]),
-                    Integer.parseInt(args[2]),
-                    Integer.parseInt(args[3]),
-                    args[4]);
-        }
-        Logger.getRootLogger()
-                .addAppender(new ConsoleAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
-        final int skiersPerThread = client.numSkiers / client.numThreads;
-        final Random random = new Random();
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-
-        long startupTime = timestamp.getTime();
+    long startupTime = timestamp.getTime();
 
 
         /*Phase 1, the startup phase, will launch numThreads/4 threads, and each thread will be passed:
@@ -111,16 +113,16 @@ public class Client {
         For example if numThreads=64 and numSkiers=1024, we will launch 16 threads, with thread 0
         passed skierID range 1 to 64, thread 1 range 65 to 128, and so on.*/
 
-        for (int i = 0; i < client.numThreads / 4; i++) {
-            Thread threadPhase1 = new Thread(client, client.numSkiers / (client.numThreads / 4), random,
-                    0.1, 1,
-                    90, 1);
-            java.lang.Thread thread1 = new java.lang.Thread(threadPhase1);
-            thread1.start();
-        }
+    for (int i = 0; i < client.numThreads / 4; i++) {
+      Thread threadPhase1 = new Thread(client, client.numSkiers / (client.numThreads / 4), random,
+          0.1, 1,
+          90, 1);
+      java.lang.Thread thread1 = new java.lang.Thread(threadPhase1);
+      thread1.start();
+    }
 
-        logger.info("Waiting for phase 1 to complete...");
-        client.phase1.await();
+    logger.info("Waiting for phase 1 to complete...");
+    client.phase1.await();
 
 
         /*Once 10% (rounded up) of the threads in Phase 1 have completed, Phase 2, the peak phase should begin. Phase 2 behaves like Phase 1, except:
@@ -131,38 +133,40 @@ public class Client {
         sends a POST request. It will do this (numRuns x 0.8)x(numSkiers/numThreads) times. Back to our
         example above, this means phase 2 would create 64 threads, and and each sends 16*(1024/64) POSTs.*/
 
-        for (int i = 0; i < client.numThreads; i++) {
-            Thread threadPhase2 = new Thread(client, skiersPerThread, random, 0.8, 91, 360, 2);
-            java.lang.Thread thread2 = new java.lang.Thread(threadPhase2);
-            thread2.start();
-        }
+    for (int i = 0; i < client.numThreads; i++) {
+      Thread threadPhase2 = new Thread(client, skiersPerThread, random, 0.8, 91, 360, 2);
+      java.lang.Thread thread2 = new java.lang.Thread(threadPhase2);
+      thread2.start();
+    }
 
-        logger.info("Waiting for phase 2 to complete...");
-        client.phase2.await();
+    logger.info("Waiting for phase 2 to complete...");
+    client.phase2.await();
 
 
         /*Finally, once 10% of the threads in Phase 2 complete, Phase 3 should begin. Phase 3, the
         cooldown phase, is identical to Phase 1, starting 25% of numThreads, with each thread sending
         (0.1 x numRuns) POST requests, and with a time interval range of 361 to 420.*/
 
-        for (int i = 0; i < client.numThreads / 4; i++) {
-            Thread threadPhase3 = new Thread(client, client.numSkiers / (client.numThreads / 4), random,
-                    0.1,
-                    361, 420, 3);
-            java.lang.Thread thread3 = new java.lang.Thread(threadPhase3);
-            thread3.start();
-        }
-        logger.info("Waiting for all phases to complete...");
-        client.total.await();
-
-        //Capture total time spent on operation
-        timestamp = new Timestamp(System.currentTimeMillis());
-        long afterPhase3 = timestamp.getTime();
-
-        String output = String.format("NumThreads = %d, numSkiers = %d, numLifts = %d", client.numThreads, client.numSkiers, client.numLifts);
-        logger.info("Configuration - " + output);
-        logger.info("Success " + client.success);
-        logger.info("failure " + client.failure);
-        logger.info("total time " + (afterPhase3 - startupTime)/1000.0 + " seconds.");
+    for (int i = 0; i < client.numThreads / 4; i++) {
+      Thread threadPhase3 = new Thread(client, client.numSkiers / (client.numThreads / 4), random,
+          0.1,
+          361, 420, 3);
+      java.lang.Thread thread3 = new java.lang.Thread(threadPhase3);
+      thread3.start();
     }
+    logger.info("Waiting for all phases to complete...");
+    client.total.await();
+
+    //Capture total time spent on operation
+    timestamp = new Timestamp(System.currentTimeMillis());
+    long afterPhase3 = timestamp.getTime();
+
+    String output = String
+        .format("NumThreads = %d, numSkiers = %d, numLifts = %d", client.numThreads,
+            client.numSkiers, client.numLifts);
+    logger.info("Configuration - " + output);
+    logger.info("Success " + client.success);
+    logger.info("failure " + client.failure);
+    logger.info("total time " + (afterPhase3 - startupTime) / 1000.0 + " seconds.");
+  }
 }
--- UpicResortsClient/src/main/java/org/core/Thread.java
+++ UpicResortsClient/src/main/java/org/core/Thread.java
@@ -1,5 +1,7 @@
 package org.core;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -11,85 +13,107 @@ import java.net.http.HttpResponse;
 import java.util.Random;
 
 public class Thread implements Runnable {
-    private static final Logger logger = LogManager.getLogger(Thread.class);
-    private static final String URL_CORE = "/skier-server/skiers/1/seasons/2018/days/22/skiers/";
-    private Client client;
-    private int skiersPerThread;
-    private Random random;
-    private double coefficient;
-    private int startTime;
-    private int endTime;
-    private int phase;
-    private HttpClient httpClient;
 
-    public Thread(Client client, int skiersPerThread, Random random, double coefficient, int startTime, int endTime, int phase) {
-        this.client = client;
-        this.skiersPerThread = skiersPerThread;
-        this.random = random;
-        this.coefficient = coefficient;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.phase = phase;
-        httpClient = HttpClient.newHttpClient();
-    }
+  private static final Logger logger = LogManager.getLogger(Thread.class);
+  private static final String URL_CORE = "/skiers/%resort_id%/seasons/2021/days/22/skiers/";
+  private Client client;
+  private int skiersPerThread;
+  private Random random;
+  private double coefficient;
+  private int startTime;
+  private int endTime;
+  private int phase;
+  private HttpClient httpClient;
 
-    @Override
-    public void run() {
+  public Thread(Client client, int skiersPerThread, Random random, double coefficient,
+      int startTime, int endTime, int phase) {
+    this.client = client;
+    this.skiersPerThread = skiersPerThread;
+    this.random = random;
+    this.coefficient = coefficient;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.phase = phase;
+    httpClient = HttpClient.newHttpClient();
+  }
+
+  @Override
+  public void run() {
+    try {
+      for (int j = 0; j < this.client.numRuns * coefficient * skiersPerThread; j++) {
+        //creating random skier id for EACH thread
+        int skierId = (random.nextInt(this.client.numSkiers) + j * this.skiersPerThread) + 1;
+
+        //choosing a random liftID
+        int liftId = random.nextInt(this.client.numLifts);
+        // 40 lifts and 5 resorts, so 8 for each resort.
+        int resortId = getResortId(liftId);
+        liftId = (liftId % 8) + 1;
+        //choosing random time period
+        int time = startTime + random.nextInt(endTime - startTime + 1);
+
         try {
-            for (int j = 0; j < this.client.numRuns * coefficient * skiersPerThread; j++) {
-                //creating random skier id for EACH thread
-                int skierId = random.nextInt(this.client.numSkiers) + j * this.skiersPerThread;
-
-                //choosing a random liftID
-                int liftId = random.nextInt(this.client.numLifts);
-
-                //choosing random time period
-                int time = startTime + random.nextInt(endTime - startTime + 1);
-
-                try {
-                    HttpResponse<String> res = doPost(httpClient, Client.IP + URL_CORE + skierId, String.format("{'liftId' : %d, 'time' : %d}", liftId, time));
-                    if (res.statusCode() == 201)
-                    {
-                        client.testSuccess();
-                    }
-                    else if (res.statusCode() >= 400 )
-                    {
-                        client.testFailure();
-                        logger.error("Failure: " + client.getFailure() + "\n caused by :" + res.body());
-                    }
+          String url = Client.IP + URL_CORE + skierId;
+          url = url.replace("%resort_id%", "" + resortId);
+          String messageBody = String.format("{\"liftId\" : %d, \"time\" : %d}", liftId, time);
+          logger.info("Calling url " + url + "\n with body - " + messageBody);
+          HttpResponse<String> res = doPost(httpClient, url, messageBody);
+          if (res.statusCode() == 201) {
+            client.testSuccess();
+          } else if (res.statusCode() >= 400) {
+            client.testFailure();
+            logger.error("Failure: " + client.getFailure() + "\n caused by :" + res.body());
+          }
 
 
-                } catch (Exception e) {
-                    client.testFailure();
-                    logger.error("Failure: " + client.getFailure());
-                    logger.error("Failed due to ", e);
-                }
-            }
-        } finally {
-            // depending on the phase, lower the countDownLatch
-            if(phase == 1) {
-                client.phase1.countDown();
-            } else if (phase == 2) {
-                client.phase2.countDown();
-            }
-            client.total.countDown();
+        } catch (Exception e) {
+          client.testFailure();
+          logger.error("Failure: " + client.getFailure());
+          logger.error("Failed due to ", e);
         }
+      }
+    } finally {
+      // depending on the phase, lower the countDownLatch
+      if (phase == 1) {
+        client.phase1.countDown();
+      } else if (phase == 2) {
+        client.phase2.countDown();
+      }
+      client.total.countDown();
     }
+  }
 
-    private static HttpResponse<String> doGet(HttpClient client, String URL) throws IOException, InterruptedException {
-        HttpRequest req = HttpRequest.newBuilder()
-                .header("accept","application/json")
-                .uri(URI.create(URL))
-                .build();
-        return client.send(req, HttpResponse.BodyHandlers.ofString());
-    }
+  private int getResortId(int liftId) {
+    return (liftId / 8) + 1;
+  }
 
-    private static HttpResponse<String> doPost(HttpClient client, String URL, String body) throws IOException, InterruptedException {
-        HttpRequest req = HttpRequest.newBuilder()
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(body))
-                .uri(URI.create(URL))
-                .build();
-        return client.send(req, HttpResponse.BodyHandlers.ofString());
+  private static HttpResponse<String> doGet(HttpClient client, String URL)
+      throws IOException, InterruptedException {
+    HttpRequest req = HttpRequest.newBuilder()
+        .header("accept", "application/json")
+        .uri(URI.create(URL))
+        .build();
+    return client.send(req, HttpResponse.BodyHandlers.ofString());
+  }
+
+  private static HttpResponse<String> doPost(HttpClient client, String URL, String body)
+      throws IOException, InterruptedException {
+    HttpRequest req = HttpRequest.newBuilder()
+        .header("Content-Type", "application/json")
+        .header("Authorization", getAuthHeaderString())
+        .POST(HttpRequest.BodyPublishers.ofString(body))
+        .uri(URI.create(URL))
+        .build();
+    return client.send(req, HttpResponse.BodyHandlers.ofString());
+  }
+
+  private static String getAuthHeaderString() {
+    try {
+      String credential = Base64.getEncoder().encodeToString(("admin:admin").getBytes("UTF-8"));
+      return "Basic " + credential.substring(0, credential.length()-1);
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
     }
+    return "";
+  }
 }
